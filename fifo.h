30e13de71b3d2a314ca7ba514dab5aed92cd23b4
XuNeo <neo.xu1990@gmail.com>
2019-02-13 15:20:25 +0800

    add License. Add FIFO source file

---
+++ fifo.h
@@ -0,0 +1,57 @@
+/**
+ * @author Neo Xu (neo.xu1990@gmail.com)
+ * @license The MIT License (MIT)
+ * 
+ * Copyright (c) 2019 Neo Xu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+*/
+#ifndef _FIFO_H_
+#define _FIFO_H_
+#include "stdint.h"
+
+#define FIFO_DIS_INT() //disableInterrupts() //disable interrupt
+#define FIFO_EN_INT()  //enableInterrupts() //enable interrupt again.
+
+typedef enum
+{
+  fifo_err_ok = 0,
+  fifo_err_full = -1,
+  fifo_err_empty = -2,
+  fifo_err_nullp = -3,  //null pointer provided.
+}fifo_err_def;
+
+typedef struct
+{
+  uint8_t *pbuff;
+  uint32_t buff_size;
+  uint32_t write_index;
+  uint32_t read_index;
+  uint32_t data_count;
+}fifo_def;
+
+void fifo_init(fifo_def *pfifo, uint8_t *pbuff, uint32_t len);
+uint32_t fifo_status(fifo_def *pfifo);
+fifo_err_def fifo_write1B(fifo_def *pfifo, uint8_t ch);
+fifo_err_def fifo_read1B(fifo_def *pfifo, uint8_t *ch);
+fifo_err_def fifo_write(fifo_def *pfifo, uint8_t *pbuff, uint32_t *plen);
+fifo_err_def fifo_read(fifo_def *pfifo, uint8_t *pbuff, uint32_t *plen);
+
+#endif
